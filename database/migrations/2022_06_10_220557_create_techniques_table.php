046afc4b98763dee9c81a5f1339bae93a750ffc2
gabriellmeireles <gabriellmeireles@hotmail.com>
2022-06-18 14:12:18 -0300

    acresentado o campo SLUG na tabela techniques

--- database/migrations/2022_06_10_220557_create_techniques_table.php
+++ database/migrations/2022_06_10_220557_create_techniques_table.php
@@ -16,6 +16,7 @@ class CreateTechniquesTable extends Migration
         Schema::create('techniques', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->string('acronym');
             $table->boolean('status');
             $table->timestamps();
